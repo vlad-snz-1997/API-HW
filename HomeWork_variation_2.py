c248daad6c43830b75c1bc9918b4f6ce6ae3071b
vlad-snz-1997 <bvd_snz@mail.ru>
2026-07-21 13:59:58 +0300

    Доработка ДЗ

--- HomeWork_variation_2.py
+++ HomeWork_variation_2.py
@@ -1,5 +1,5 @@
 import requests
-from io import StringIO
+from io import BytesIO
 import json
 import os
 from dotenv import load_dotenv
@@ -14,30 +14,35 @@ class IPDetector:
         self.url_get_ip = 'https://api.ipify.org/?format=json'
         self.response=requests.get(self.url_get_ip)
         self.ip_number = self.response.json()['ip']
-        ip = self.response.json()['ip']
-        return ip
+        self.ip = self.response.json()['ip']
+        print(self.ip)# не работает принт
+        return self.ip
 
 class IPInfo:
-    def __init__(self):
+    def __init__(self,ip):
         pass
 
-    def get_info_ip(self):
-        self.url_ip_info = f'https://ipinfo.io/{ip}/geo'
+    def get_info_ip(self,ip):
+        self.url_ip_info = f'https://ipinfo.io/{self.ip}/geo'
         self.response=requests.get(self.url_ip_info)
         self.info_ip = self.response.json()
         
         # Создаём байтовый буфер
-        bytes_buffer = StringIO ()
-        # Сериализуем данные в JSON и сразу записываем в буфер
-        json.dump(self.info_ip, bytes_buffer)
-        # Получаем байты из буфера
-        json_bytes = bytes_buffer.getvalue()
-        bytes_buffer.close()
-        print(self.info_ip)
-        return json_bytes
+        buffer = BytesIO()
+        # Сериализуем данные в JSON 
+        self.json_srting = json.dumps(self.info_ip).encode('utf-8')
+        # записываем сериалезованные данные в буфер
+        buffer.write(self.json_bytes)
+        # Извлекаем байты из потока в переменную
+        self.json_bytes = buffer.getvalue() 
+        # ОШИБКА    TypeError: a bytes-like object is required, not 'IPInfo'
+        print(self.json_bytes) # не работает принт
+        buffer.close()
+        return self.json_bytes # возвращаем байтовый объект
 
 class YandexUploader:
-    def __init__(self,token):
+    def __init__(self,token,json_bytes):
+        self.json_bytes = json_bytes
         self.url_yandex_disk = 'https://cloud-api.yandex.net/'
         self.headers = {'Authorization': f'OAuth {token}'}
         
@@ -50,21 +55,19 @@ class YandexUploader:
             print('Папка создана')
         return  response.status_code == 201
 
-    def upload_file(self,path_file,path_yd):
-        
+    def upload_file(self,json_bytes,path_file,path_yd):
+        self.json_bytes = json_bytes
         self.response=requests.get(f'{self.url_yandex_disk}v1/disk/resources/upload',headers=self.headers,params={'path':path_yd,'overwrite':True})
         url_put_file = self.response.json()['href']
-        self.response=requests.put(url_put_file,headers=self.headers,files={'file':(path_file, json_bytes)})
+        self.response=requests.put(url_put_file,headers=self.headers,files={'file':(path_file, self.json_bytes)})
         print(f'файл создан')
         return self.response.status_code == 200
 
 
 if __name__ == '__main__':
     api_get_ip = IPDetector()
-    ip = api_get_ip.get_ip() # извлекаем ip из функции
-    api_ip_info = IPInfo()
-    json_bytes = api_ip_info.get_info_ip()  # извлекаем информацию об ip из функции
+    api_ip_info = IPInfo(api_get_ip)
     token = os.getenv('TOKEN')
-    api_yandex_disk = YandexUploader(token) # авторизуемся на яндекс диске
+    api_yandex_disk = YandexUploader(token,api_ip_info) # авторизуемся на яндекс диске
     api_yandex_disk.create_folder('ip_info') # создаем папку 
-    api_yandex_disk.upload_file('info_ip.txt','ip_info/info_ip.txt') # загружаем файл 
+    api_yandex_disk.upload_file(api_ip_info,'info_ip.txt','ip_info/info_ip.txt') # загружаем файл 
